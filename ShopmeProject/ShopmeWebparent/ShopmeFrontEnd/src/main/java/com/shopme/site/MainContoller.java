19eb9b0c10a15d3a1957c55807cef95436a32733
naris12 <naris.bin1@gmail.com>
2022-08-15 19:09:32 +0700

    first time

---
+++ ShopmeProject/ShopmeWebparent/ShopmeFrontEnd/src/main/java/com/shopme/site/MainContoller.java
@@ -0,0 +1,12 @@
+package com.shopme.site;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainContoller {
+    @GetMapping("")
+    public String viewHomepage(){
+        return "index";
+    }
+}
